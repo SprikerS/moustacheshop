07d2b9bf36636f06738e17d7ab22e19fb1a87840
SprikerS <jeremyestelo17@gmail.com>
2024-10-14 18:32:58 -0500

    view of the finished user module

---
+++ src/main/java/dev/sprikers/moustacheshop/controllers/UserController.java
@@ -0,0 +1,63 @@
+package dev.sprikers.moustacheshop.controllers;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXCheckBox;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.image.ImageView;
+import org.controlsfx.control.CheckComboBox;
+
+import dev.sprikers.moustacheshop.helpers.PasswordToggleManager;
+
+public class UserController implements Initializable {
+
+    @FXML
+    private CheckComboBox<String> chkcbRoles;
+
+    @FXML
+    private ImageView imgToggleEye;
+
+    @FXML
+    private JFXButton btnClean, btnDelete, btnSubmit;
+
+    @FXML
+    private JFXCheckBox chkActive;
+
+    @FXML
+    private PasswordField txtHiddenPass;
+
+    @FXML
+    private TextField txtDNI, txtEmail, txtMaternalSurname, txtNames, txtPaternalSurname, txtPhone, txtVisiblePass;
+
+    @FXML
+    private ToggleButton toggleDisplayPass;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        PasswordToggleManager.configureVisibility(txtHiddenPass, txtVisiblePass, toggleDisplayPass, imgToggleEye);
+
+        ObservableList<String> validRoles = FXCollections.observableArrayList(
+            "Cliente",
+            "Trabajador",
+            "Administrador"
+        );
+
+        chkcbRoles.getItems().addAll(validRoles);
+        chkcbRoles.getCheckModel().check(0);
+    }
+
+    private List<String> getSelectedRoles() {
+        return new ArrayList<>(chkcbRoles.getCheckModel().getCheckedItems());
+    }
+
+}
