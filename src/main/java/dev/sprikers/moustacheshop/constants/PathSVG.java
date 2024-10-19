5672def0a16f0ecdac4a2374d260f669b388a543
SprikerS <jeremyestelo17@gmail.com>
2024-10-19 13:22:10 -0500

    integration of the fxml view of the order module

--- src/main/java/dev/sprikers/moustacheshop/constants/PathSVG.java
+++ src/main/java/dev/sprikers/moustacheshop/constants/PathSVG.java
@@ -6,6 +6,7 @@ public class PathSVG {
 
     public static final String BOLT = SVG_PATH + "bolt.svg";
     public static final String LAYOUT_GRID = SVG_PATH + "layout-grid.svg";
+    public static final String NOTEBOOK_TEXT = SVG_PATH + "notebook-text.svg";
     public static final String PACKAGE = SVG_PATH + "package.svg";
     public static final String USERS_ROUND = SVG_PATH + "users-round.svg";
 
